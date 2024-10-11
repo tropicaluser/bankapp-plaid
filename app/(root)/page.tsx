c5e1726373cb0df85853ef9f5fbcca16d3274621
tropicaluser <hallbergstephaniie@gmail.com>
2024-10-11 11:26:02 +0200

    4. sidebar - add right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,37 +1,40 @@
-import HeaderBox from '@/components/HeaderBox'
-import TotalBalanceBox from '@/components/TotalBalanceBox';
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  const loggedIn = { firstName: 'Stephanie', lastName: 'JSM', email: 'contact@jsmastery.pro' };
+  const loggedIn = {
+    firstName: "Stephanie",
+    lastName: "JSM",
+    email: "contact@jsmastery.pro",
+  };
 
   return (
     <section className="home">
       <div className="home-content">
         <header className="home-header">
-          <HeaderBox 
+          <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || 'Guest'}
+            user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
 
-          <TotalBalanceBox 
+          <TotalBalanceBox
             accounts={[]}
             totalBanks={1}
             totalCurrentBalance={1250.35}
           />
         </header>
-
         RECENT TRANSACTIONS
       </div>
 
-      {/* <RightSidebar 
+      <RightSidebar
         user={loggedIn}
-        transactions={[]}
-        banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50}]}
-      /> */}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+      />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
